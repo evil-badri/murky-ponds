aeb7c2446c28b800af3c3fbbc1a1c04390cb7c74
Badrinath Janakiraman <bjanakir@thoughtworks.com>
2012-08-09 11:41:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MurkyPonds::Application.config.secret_token = 'c868853f56a6cc676778ef3c702789aa7737e73e26ac1d49bdf7ee948b773f71fa8eb68efacd5d79292d5bff1582bd5a3f3f7d524b6311bddbcef71ab2d4fcfa'
